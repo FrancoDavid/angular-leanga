2540f7b676709994fd8d7fe44fc35235b328ea42
Redrum <franco.avalos@live.com>
2021-02-06 16:28:35 -0300

    feat: component list-post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListPostComponent } from './components/list-post/list-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: ListPostComponent
+  },
+  {
+      path: '',
+      redirectTo: 'posts',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: 'posts'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
